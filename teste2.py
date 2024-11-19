94cd68055ba96239da5951440c14a56d995d097a
HeyLo <150632506+Heylinho@users.noreply.github.com>
2024-11-19 18:01:36 -0300

    Add files via upload

---
+++ teste2.py
@@ -0,0 +1,8 @@
+escreva = input('Escreva algo: ')
+
+print('Tipo número? ', escreva.isnumeric())
+print('Tipo Aplhabeto? ',escreva.isalpha())
+print('Tipo Aplhabeto MAIUSCULO? ',escreva.isupper())
+print('Tipo só tem espaço? ',escreva.isspace())
+print('Tipo capitalizada? ',escreva.istitle())
+
